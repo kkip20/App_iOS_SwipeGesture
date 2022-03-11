78249ef1d0ae8d68fa06748dd72379e55a497e55
Kyung Hoon Kim <kyunghoonkim@Kyungui-MacBookPro.local>
2022-03-11 23:37:42 +0900

    add comments

--- App_iOS_SwipeGesture/ViewController.swift
+++ App_iOS_SwipeGesture/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
     @IBOutlet var imgViewRight: UIImageView!
     @IBOutlet var imgViewDown: UIImageView!
     
+    // imageを保管する配列
     var imgLeft = [UIImage]()
     var imgRight = [UIImage]()
     var imgUp = [UIImage]()
@@ -24,57 +25,114 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
+        // 各配列にimage追加
         imgUp.append(UIImage(named: "arrow-up-black.png")!)
         imgUp.append(UIImage(named: "arrow-up-red.png")!)
+        imgUp.append(UIImage(named: "arrow-up-green.png")!)
         imgDown.append(UIImage(named: "arrow-down-black.png")!)
         imgDown.append(UIImage(named: "arrow-down-red.png")!)
+        imgDown.append(UIImage(named: "arrow-down-green.png")!)
         imgLeft.append(UIImage(named: "arrow-left-black.png")!)
         imgLeft.append(UIImage(named: "arrow-left-red.png")!)
+        imgLeft.append(UIImage(named: "arrow-left-green.png")!)
         imgRight.append(UIImage(named: "arrow-right-black.png")!)
         imgRight.append(UIImage(named: "arrow-right-red.png")!)
+        imgRight.append(UIImage(named: "arrow-right-green.png")!)
         
+        // 四つのimageViewに初期image（黒）保存
         imgViewUp.image = imgUp[0]
         imgViewDown.image = imgDown[0]
         imgViewLeft.image = imgLeft[0]
         imgViewRight.image = imgRight[0]
         
+        // 一つの指swipe gesture登録（上、下、左、右）
         let swipeUp = UISwipeGestureRecognizer(target: self, action: #selector(ViewController.respondToSwipeGesture(_:)))
         swipeUp.direction = UISwipeGestureRecognizer.Direction.up
-        swipeUp.numberOfTouchesRequired = numOfTouches
+//        swipeUp.numberOfTouchesRequired = numOfTouches
         self.view.addGestureRecognizer(swipeUp)
         
         let swipeDown = UISwipeGestureRecognizer(target: self, action: #selector(ViewController.respondToSwipeGesture(_:)))
         swipeDown.direction = UISwipeGestureRecognizer.Direction.down
-        swipeDown.numberOfTouchesRequired = numOfTouches
+//        swipeDown.numberOfTouchesRequired = numOfTouches
         self.view.addGestureRecognizer(swipeDown)
         
         let swipeLeft = UISwipeGestureRecognizer(target: self, action: #selector(ViewController.respondToSwipeGesture(_:)))
         swipeLeft.direction = UISwipeGestureRecognizer.Direction.left
-        swipeLeft.numberOfTouchesRequired = numOfTouches
+//        swipeLeft.numberOfTouchesRequired = numOfTouches
         self.view.addGestureRecognizer(swipeLeft)
         
         let swipeRight = UISwipeGestureRecognizer(target: self, action: #selector(ViewController.respondToSwipeGesture(_:)))
         swipeRight.direction = UISwipeGestureRecognizer.Direction.right
-        swipeRight.numberOfTouchesRequired = numOfTouches
+//        swipeRight.numberOfTouchesRequired = numOfTouches
         self.view.addGestureRecognizer(swipeRight)
+        
+        // 二つの指swipe gesture登録（上、下、左、右）
+        let swipeUpMulti = UISwipeGestureRecognizer(target: self, action: #selector(ViewController.respondToSwipeGestureMulti(_:)))
+        swipeUpMulti.direction = UISwipeGestureRecognizer.Direction.up
+        swipeUpMulti.numberOfTouchesRequired = numOfTouches
+        self.view.addGestureRecognizer(swipeUpMulti)
+        
+        let swipeDownMulti = UISwipeGestureRecognizer(target: self, action: #selector(ViewController.respondToSwipeGestureMulti(_:)))
+        swipeDownMulti.direction = UISwipeGestureRecognizer.Direction.down
+        swipeDownMulti.numberOfTouchesRequired = numOfTouches
+        self.view.addGestureRecognizer(swipeDownMulti)
+        
+        let swipeLeftMulti = UISwipeGestureRecognizer(target: self, action: #selector(ViewController.respondToSwipeGestureMulti(_:)))
+        swipeLeftMulti.direction = UISwipeGestureRecognizer.Direction.left
+        swipeLeftMulti.numberOfTouchesRequired = numOfTouches
+        self.view.addGestureRecognizer(swipeLeftMulti)
+        
+        let swipeRightMulti = UISwipeGestureRecognizer(target: self, action: #selector(ViewController.respondToSwipeGestureMulti(_:)))
+        swipeRightMulti.direction = UISwipeGestureRecognizer.Direction.right
+        swipeRightMulti.numberOfTouchesRequired = numOfTouches
+        self.view.addGestureRecognizer(swipeRightMulti)
+        
     }
     
+    // 一つの指swipe gesture時に実行するaction method
     @objc func respondToSwipeGesture(_ gesture: UIGestureRecognizer) {
+        // gestureがあった場合
+        if let swipeGesture = gesture as? UISwipeGestureRecognizer {
+            // 四つのimageViewに初期image（黒）保存
+            imgViewUp.image = imgUp[0]
+            imgViewDown.image = imgDown[0]
+            imgViewLeft.image = imgLeft[0]
+            imgViewRight.image = imgRight[0]
+            
+            // 発生したeventが各方向のswipe eventなら該当imageViewを赤imageに変更
+            switch swipeGesture.direction {
+                case UISwipeGestureRecognizer.Direction.up:
+                    imgViewUp.image = imgUp[1]
+                case UISwipeGestureRecognizer.Direction.down:
+                    imgViewDown.image = imgDown[1]
+                case UISwipeGestureRecognizer.Direction.left:
+                    imgViewLeft.image = imgLeft[1]
+                case UISwipeGestureRecognizer.Direction.right:
+                    imgViewRight.image = imgRight[1]
+                default:
+                    break
+            }
+        }
+    }
+    
+    // 二つの指swipe gesture時に実行するaction method
+    @objc func respondToSwipeGestureMulti(_ gesture: UIGestureRecognizer) {
         if let swipeGesture = gesture as? UISwipeGestureRecognizer {
             imgViewUp.image = imgUp[0]
             imgViewDown.image = imgDown[0]
             imgViewLeft.image = imgLeft[0]
             imgViewRight.image = imgRight[0]
             
+            // 発生したeventが各方向のswipe eventなら該当imageViewを緑imageに変更
             switch swipeGesture.direction {
             case UISwipeGestureRecognizer.Direction.up:
-                imgViewUp.image = imgUp[1]
+                imgViewUp.image = imgUp[2]
             case UISwipeGestureRecognizer.Direction.down:
-                imgViewDown.image = imgDown[1]
+                imgViewDown.image = imgDown[2]
             case UISwipeGestureRecognizer.Direction.left:
-                imgViewLeft.image = imgLeft[1]
+                imgViewLeft.image = imgLeft[2]
             case UISwipeGestureRecognizer.Direction.right:
-                imgViewRight.image = imgRight[1]
+                imgViewRight.image = imgRight[2]
             default:
                 break
             }
